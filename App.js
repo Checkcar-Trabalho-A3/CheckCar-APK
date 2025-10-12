06ba9e30f0d44a94517c467551c29dc09f70f599
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-10-11 22:18:19 -0300

    style: layout tela de login

--- App.js
+++ App.js
@@ -5,16 +5,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import ChecklistScreen from './screens/ChecklistScreen';
 import SummaryScreen from './screens/SummaryScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options ={{ headerShown: false }}/>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Checklist" component={ChecklistScreen} />
-        <Stack.Screen name="SummaryScreen" component={SummaryScreen} />
+        <Stack.Screen name="Summary" component={SummaryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/LoginScreen.js
@@ -0,0 +1,149 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, TextInput, TouchableOpacity, Text } from 'react-native';
+import Svg, { Defs, RadialGradient, Rect, Stop } from 'react-native-svg';
+import { useFonts, Outfit_300Light } from '@expo-google-fonts/outfit';
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [fontsLoaded] = useFonts({
+    Outfit_300Light,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Fundo radial */}
+      <Svg height="100%" width="100%" style={StyleSheet.absoluteFill}>
+        <Defs>
+          <RadialGradient
+            id="grad"
+            cx="50%"
+            cy="50%"
+            rx="50%"
+            ry="50%"
+            fx="50%"
+            fy="50%"
+          >
+            <Stop offset="0%" stopColor="#474747" stopOpacity="1" />
+            <Stop offset="100%" stopColor="#0F0F0F" stopOpacity="1" />
+          </RadialGradient>
+        </Defs>
+        <Rect x="0" y="0" width="100%" height="100%" fill="url(#grad)" />
+      </Svg>
+
+      {/* Imagem no topo esquerdo */}
+      <View style={styles.topLeftImage}>
+        <Image
+          source={require('../assets/car.png')}
+          style={styles.carImage}
+          resizeMode="contain"
+        />
+      </View>
+
+      {/* Conteúdo da tela */}
+      <View style={styles.content}>
+        <Image
+          source={require('../assets/logintext.png')}
+          style={styles.logintext}
+          resizeMode="contain"
+        />
+
+        {/* Inputs */}
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu e-mail"
+          placeholderTextColor="#ccc"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          placeholderTextColor="#ccc"
+          value={senha}
+          onChangeText={setSenha}
+          secureTextEntry
+        />
+
+        {/* Botões */}
+        <TouchableOpacity style={styles.loginButton} onPress={() => console.log('Fazer login')}>
+          <Text style={styles.loginButtonText}>Entrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => console.log('Esqueci minha senha')}>
+          <Text style={styles.forgotText}>Esqueci minha senha</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
+  topLeftImage: {
+    position: 'absolute',
+    top: 40,
+    left: 20,
+    zIndex: 10,
+  },
+  carImage: {
+    width: 50,
+    height: 100,
+    marginLeft: 10,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingTop: 280,
+  },
+  logintext: {
+    width: 110,
+    height: 90,
+    marginBottom: 1,
+  },
+  input: {
+    backgroundColor: '#5A5A5A',
+    color: '#fff',
+    fontFamily: 'Outfit_300Light',
+    padding: 15,
+    borderRadius: 8,
+    marginBottom: 15,
+    width: '85%',
+    minHeight: 50,
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  loginButton: {
+    backgroundColor: '#5A5A5A',
+    paddingVertical: 10,
+    borderRadius: 8,
+    width: '40%',
+    alignItems: 'center',
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 15,
+    fontFamily: 'Outfit_300Light',
+  },
+  forgotText: {
+    color: '#ccc',
+    fontSize: 14,
+    marginTop: 12,
+    fontFamily: 'Outfit_300Light',
+  },
+});
